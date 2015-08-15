f7a3fff2afbe49562920108a8686a4c4e8eafd6f
Serfgey Glazyrin <sergey.glazyrin.dev@gmail.com>
2015-08-15 02:33:27 +0200

    updated way how to store structure of the home directory

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 
-cp -r Home/* ~/
+CUR_DIR=`pwd`
+HOME_DIR=`dirname ~/`/$USER
+HOME_STRUCTURE_ARCHIVE="$CUR_DIR/home.tar.gz"
+/bin/tar --skip-old-files -xvzf $HOME_STRUCTURE_ARCHIVE --directory $HOME_DIR/
 
-if [[ ! -f ~/.emacs.d/init.el ]]; then
+if [[ ! -h ~/.emacs.d/init.el ]]; then
    mkdir ~/.emacs.d/
    ln -s ~/Projects/Personal/myworkenv/init.el ~/.emacs.d/
 fi
