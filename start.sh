59413f797871bbc560611b4c575dbdede630bc27
Serfgey Glazyrin <sergey.glazyrin.dev@gmail.com>
2015-08-15 02:36:39 +0200

    bash comments

--- start.sh
+++ start.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 
+
 CUR_DIR=`pwd`
 HOME_DIR=`dirname ~/`/$USER
+
+# keep home directory structure the same everywhere
 HOME_STRUCTURE_ARCHIVE="$CUR_DIR/home.tar.gz"
 /bin/tar --skip-old-files -xvzf $HOME_STRUCTURE_ARCHIVE --directory $HOME_DIR/
 
+# emacs beautifying
 if [[ ! -h ~/.emacs.d/init.el ]]; then
    mkdir ~/.emacs.d/
    ln -s ~/Projects/Personal/myworkenv/init.el ~/.emacs.d/
